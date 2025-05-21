83c3fb8958d6aac75225437de540541536705496
TommyWeemox <158041944+TommyWeemox@users.noreply.github.com>
2025-05-21 12:45:27 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def hello_world():
     return render_template('hello.html')  # Fichier hello.html requis dans /templates
 
-# Génère une clé globalhhe (utilisée dans /encrypt/<valeur> et /decrypt/<valeur>)
+# Génère une clé globalhe (utilisée dans /encrypt/<valeur> et /decrypt/<valeur>)
 global_key = Fernet.generate_key()
 global_f = Fernet(global_key)
 
